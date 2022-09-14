10a7c0e8d125de236f5b871bad3aa0145d9d7dc9
Wyatt <you@example.com>
2022-09-13 20:46:54 -0400

    new file:   __pycache__/blockchain.cpython-310.pyc 	modified:   blockchain.py 	modified:   main.py

--- blockchain.py
+++ blockchain.py
@@ -1,9 +1,6 @@
-from flask import Flask, jsonify
 import json
 import datetime
 import hashlib
-
-
 
 class Blockchain:
 
@@ -83,46 +80,3 @@ class Blockchain:
         return True
 
 
-app = Flask(__name__)
-
-blockchain = Blockchain()
-
-@app.route('/mine_block', methods=['GET'])
-def mine_block():
-    previousBlock = blockchain.print_previous_block()
-    previousProof = previousBlock['proof']
-    proof = blockchain.proofWork(previousProof)
-    previousHash = blockchain.hash(previousBlock)
-    block = blockchain.createBlock(proof, previousHash)
-
-    response = {
-        'message': "A block is Mined",
-        'index': block['index'],
-        'timestamp': block['timestamp'],
-        'proof': block['proof'],
-        'previous_hash': block['previous_hash']
-    }
-
-    return jsonify(response), 200
-
-@app.route('/get_chain', methods=['GET'])
-def display_chain():
-    response = {
-        'chain': blockchain.chain,
-        'length': len(blockchain.chain)
-    }
-    
-    return jsonify(response), 200
-
-
-@app.route('/valid', methods = ['GET'])
-def valid():
-    valid = blockchain.chainValid(blockchain.chain)
-
-    if valid:
-        response = {'message': 'The Blockchain is valid'}
-    else:
-        response = {'message': 'The Blockchain is NOT valid'}
-    return jsonify(response), 200
-
-app.run(host='127.0.0.1', port=5000)
